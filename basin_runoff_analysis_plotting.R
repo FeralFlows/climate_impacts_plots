a564124d0f3f7e184822beca962fbcdee3df6c09
FeralFlows <twild@umd.edu>
2019-07-31 18:43:13 -0400

    adding features such as removing certain basins from analysis

--- basin_runoff_analysis_plotting.R
+++ basin_runoff_analysis_plotting.R
@@ -27,7 +27,7 @@ gcm_colors <- c("NorESM1-M" = "#736F6E",
                 'historical' = 'black',
                 'historical mean' = 'black')
 figures_basepath <- 'C:/Users/twild/all_git_repositories/idb_results/downscaling/Water/Xanthos/output/figures/basin'
-results_basepath <- 'C:/Users/twild/all_git_repositories/Xanthos_final_4/example/output'
+results_basepath <- 'C:/Users/twild/all_git_repositories/Xanthos_python3/xanthos/example/output'
 xanthos_config_names <- c('clim_impacts')
 gcm_names <- c('NorESM1-M', 'MIROC-ESM-CHEM', 'IPSL-CM5A-LR', 'HadGEM2-ES', 'GFDL-ESM2M')
 gcm_names_incl_hist <- append(gcm_names, 'watch+wfdei')
@@ -213,38 +213,45 @@ for(var_1 in xanthos_var_names){
 # Plot all of above lines that appear in facet, but all on the same plot
 y_ax_lbl <- expression(Annual~Runoff~(km^3))
 #input <- runoff_gcm_all_GCAM_fut %>% filter(year>=2010, year<=2050) %>% mutate(smoothedY=clim_imp_val)
+start_yr_hist <- 1990
+end_yr_hist <- 2010
 input <- df_2_all_runs_basin %>% filter(year>=2010, year<=2050) %>% mutate(smoothedY=clim_imp_val)
 runoff_gcm_all_GCAM_hist <- df_2_all_runs_basin %>% filter(year>=start_yr_hist, year<=end_yr_hist_mod) %>%
   mutate(rcp='historical mean', gcm='historical mean') %>% mutate(smoothedY=mean_hist)
 roll <- 2
 start_yr <- 2010
 end_yr <- 2050
-start_yr_hist <- 1970
-end_yr_hist <- 2010
 var_names <- c('Basin_runoff_km3peryear')
 region_list <- water_basins_plot
-gcm_list <- 'GFDL-ESM2M'
-rcp_list <- c('rcp8p5', 'rcp2p6')
+gcm_list <- 'GFDL-ESM2M' # IPSL-CM5A-LR'
+rcp_list <- c('rcp2p6', 'rcp8p5')
 region_single_plot(xanthos_var_names, region_list, input, figures_basepath, start_yr, end_yr, gcm_names, rcp_names,
                    roll, y_ax_lbl, trendline=0, combined_lines=1, plot_df_hist=runoff_gcm_all_GCAM_hist,
-                   all_same_color = 0, titles = 'Yes', legend_on=F, gcm_list=gcm_list, rcp_list=rcp_list)
+                   all_same_color = 1, titles = 'Yes', legend_on=F, gcm_list=gcm_list, rcp_list=rcp_list)
 
 # Plot percentage reduction in smoothed runoff compared with 2010
-y_ax_lbl <- expression(atop(Change~('%')~'in'~runoff,
-                            ~from~2010))
-input <- df_2_all_runs_basin %>% filter(year>=2010, year<=2050)
-input <- input %>% mutate(smoothedY=clim_imp_perc)
+y_ax_lbl <- expression(Change~('%')~'from'~2010~runoff)
+#y_ax_lbl <- expression(atop(Change~('%')~'from'~2010~runoff,
+#                            ~from~2010))
+
+input <- df_2_all_runs_basin %>% filter(year>=2010, year<=2050) %>% 
+  mutate(clim_imp_perc=if_else(year<2010, 0,clim_imp_perc)) %>% 
+  mutate(smoothedY=clim_imp_perc)
+hist_temp <- df_2_all_runs_basin %>% filter(year<=2010, year>=1990) %>% mutate(clim_imp_perc=0)
+
 roll <- 2
 start_yr <- 2010
 end_yr <- 2050
-start_yr_hist <- 1970
+start_yr_hist <- 1990
 end_yr_hist <- 2010
 var_names <- c('Basin_runoff_km3peryear')
 region_list <- water_basins_plot
+gcm_list <- 'GFDL-ESM2M'  #  'IPSL-CM5A-LR'
+rcp_list <- c('rcp2p6', 'rcp8p5')
 region_single_plot(var_names, region_list, input, figures_basepath, start_yr, end_yr, gcm_names, rcp_names,
-                   roll, y_ax_lbl, trendline=0, combined_lines=1, plot_df_hist=df_all_runs_basin_hist,
-                   all_same_color = 0, titles = 'Yes', legend_on=F, plot_hist=FALSE, plot_var='perc_red',
-                   xmin=2010, xmax=2050)
+                   roll, y_ax_lbl, trendline=0, combined_lines=1, plot_df_hist=hist_temp,
+                   all_same_color = 1, titles = 'Yes', legend_on=F, plot_hist=FALSE, plot_var='perc_red',
+                   xmin=2010, xmax=2050, gcm_list=gcm_list, rcp_list=rcp_list)
 
 # Having produced all plots, now save file as csv, in format that will allow it to be converted into gcam-ready xml
 variable <- 'runoff'
@@ -253,9 +260,10 @@ renewrsc_max_gcam <- paste0(extras_dir, '/', "L201.RenewRsrcCurves_calib_waterga
 read_csv(renewrsc_max_gcam, skip = 4) %>%
   select(region, renewresource) %>% unique() -> region_basin
 df_2_all_runs_basin <- df_2_all_runs_basin
+name_exclude_list <- c('Andamaman-Nicobar Islands', 'North Marina Islands-Guam', 'Micronesia')
 gcam_xanthos_basin_mapping <- 'C:/Users/twild/all_git_repositories/idb_results/downscaling/Water/Xanthos/output/gcam_xanthos_basin_mapping.csv'
 write_csv_file(df_2_all_runs_basin, gcam_years, gcm_names, rcp_names, level2outdir, variable, region_basin=region_basin,
-               gcam_xanthos_basin_mapping=gcam_xanthos_basin_mapping)
+               gcam_xanthos_basin_mapping=gcam_xanthos_basin_mapping, name_exclude_list=name_exclude_list)
 
 # Convert csv files to gcam-ready xml files
 var <- "GrdRenewRsrcMax" #  "AgProdChange"
